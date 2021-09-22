3b476d595da7365a05b57a4477673bd921d69777
qvv214 <qvv214@gmail.com>
2021-09-22 23:48:32 +0300

    version_8

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,6 +3,6 @@ package main.java;
 public class Main {
     
     public static void main(String[] args) {
-        System.out.println("TEST_BRANCH version_7");
+        System.out.println("TEST_BRANCH version_8");
     }
 }
