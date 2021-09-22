dd6c39b3fe1976c5cec1e892b26f66fe2f97f040
qvv214 <qvv214@gmail.com>
2021-09-22 22:32:46 +0300

    version_6

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,6 +3,6 @@ package main.java;
 public class Main {
     
     public static void main(String[] args) {
-        System.out.println("DEVELOP version_6");
+        System.out.println("DEVELOP version_7");
     }
 }
